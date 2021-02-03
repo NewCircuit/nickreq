e46726985e01786387b5a939e7dc0b2d692db0dc
Roman <rexmaster326@outlook.com>
2021-02-02 17:47:19 -0800

    added messages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { CommandoClient } from "discord.js-commando"
-
+import { Message } from "discord.js"
 const client = new CommandoClient({
     commandPrefix: ".nick"
 })
 
 client.on("ready", () => {
     console.log(`Ready as ${client.user?.tag}`)
+})
+client.on("message", (msg: Message) => {
+    console.log(msg.content);
 })
 
 client.login("");
