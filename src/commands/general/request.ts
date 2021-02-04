6273f58d2147e3d472a622445545abecde6dd70e
DankDumpster <staalkoper@gmail.com>
2021-02-04 02:21:09 +0100

    added args

--- src/commands/general/request.ts
+++ src/commands/general/request.ts
@@ -3,10 +3,17 @@ import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 export default class Request extends Command {
   constructor(client: CommandoClient) {
     super(client, {
-      description: '',
-      group: '',
-      memberName: '',
-      name: '',
+      description: 'Request a nickname',
+      group: 'general',
+      memberName: 'request',
+      name: 'request',
+      args: [
+        {
+          prompt: 'The nickname to be requested',
+          key: 'nickname',
+          type: 'string',
+        },
+      ],
     });
   }
 
