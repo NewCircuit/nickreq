e28682eeaa488e0dd38a114698bb494ce2654df3
Roman <Traveeleer@outlook.com>
2021-02-04 22:22:09 -0800

    Hi Matthew

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { CommandoClient } from 'discord.js-commando';
 import path from 'path';
 import dotenv from 'dotenv';
+import { MessageEmbed } from 'discord.js';
 
 dotenv.config();
 
 const client = new CommandoClient({
   commandPrefix: '.nick',
+ 
+
+  
 });
+
+
 
 client.on('ready', () => {
   console.log(`Ready as ${client.user?.tag}`);
@@ -18,5 +24,8 @@ client.registry
   ])
   .registerDefaults()
   .registerCommandsIn(path.join(__dirname, 'commands'));
+
+
+ 
 
 client.login(process.env.DISCORD_TOKEN);
