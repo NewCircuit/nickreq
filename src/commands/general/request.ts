feb9ce5af3c853f565d98c63772b5f95695d2fae
DankDumpster <staalkoper@gmail.com>
2021-02-07 02:10:25 +0100

    refactoring

--- src/commands/general/request.ts
+++ src/commands/general/request.ts
@@ -1,7 +1,6 @@
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 import { MessageEmbed, TextChannel } from 'discord.js';
 
-
 export default class Request extends Command {
   constructor(client: CommandoClient) {
     super(client, {
@@ -20,38 +19,31 @@ export default class Request extends Command {
   }
 
   public async run(msg: CommandoMessage, { nickname }: { nickname: string }): Promise<null> {
-    
-const embed = new MessageEmbed();
-embed.title = "Nickname Request";
-embed.description = nickname;
-embed.footer = {
-  text: 'Request sent!',
-  iconURL: 'https://brianmunoz.co/pewdiepie/images/pew-thumbsup.png',
-};
- embed.color = 0x62bd0d;
- embed.author = {
-  name: 'Success!',
-  iconURL: '',
-};
+    const embed = new MessageEmbed();
+    embed.title = 'Nickname Request';
+    embed.description = nickname;
 
-await msg.channel.send(embed);
-if (process.env.CHANNEL_ID === undefined) {
-  return null
-  
-}
+    embed.footer = {
+      text: 'Request sent!',
+      iconURL: 'https://brianmunoz.co/pewdiepie/images/pew-thumbsup.png',
+    };
 
-const channel = await this.client.channels.fetch(process.env.CHANNEL_ID) as TextChannel;
+    embed.color = 0x62bd0d;
+    embed.author = {
+      name: 'Success!',
+      iconURL: '',
+    };
 
+    await msg.channel.send(embed);
 
-channel.send ("Request")
-embed.title= "Nickname request"
-embed.description = "Nickname Request for user"
+    if (process.env.CHANNEL_ID === undefined) {
+      return null;
+    }
 
+    const channel = await this.client.channels.fetch(process.env.CHANNEL_ID) as TextChannel;
 
-
-
+    channel.send('Request');
 
     return null;
-    
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import { CommandoClient } from 'discord.js-commando';
 import path from 'path';
 import dotenv from 'dotenv';
-import { MessageEmbed } from 'discord.js';
 
 dotenv.config();
 
 const client = new CommandoClient({
   commandPrefix: '.nick',
- 
-
-  
 });
-
-
 
 client.on('ready', () => {
   console.log(`Ready as ${client.user?.tag}`);
@@ -24,8 +18,5 @@ client.registry
   ])
   .registerDefaults()
   .registerCommandsIn(path.join(__dirname, 'commands'));
-
-
- 
 
 client.login(process.env.DISCORD_TOKEN);
